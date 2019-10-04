4195ab17629bd805b8c4df5e9bde573fd3a9a101
Mai Ly Le <94mailyle@gmail.com>
2019-10-04 11:09:49 +0200

    set up layout for AgainstNazisInfoPage

--- src/containers/AgainstNazisInfoContainer/AgainstNazisInfoPage.js
+++ src/containers/AgainstNazisInfoContainer/AgainstNazisInfoPage.js
@@ -1,7 +1,102 @@
 import React from 'react';
+import { Card, CardMedia, CardContent, Typography } from '@material-ui/core/';
+import { makeStyles } from '@material-ui/styles';
+import withWidth, { isWidthUp } from '@material-ui/core/withWidth';
+import imageInfoPage from '../../assets/imageInfoPage.png';
 
-function AgainstNazisInfoPage() {
-  return <div></div>;
+const useStyles = makeStyles(theme => ({
+  root: {
+    position: 'absolute',
+    left: '50%',
+    top: '50%',
+    transform: 'translate(-50%, -50%)',
+    justifyContent: 'center'
+  },
+  card: {
+    margin: 'auto',
+    marginTop: 100,
+    [theme.breakpoints.up('md')]: {
+      display: 'flex'
+    },
+    [theme.breakpoints.only('md')]: {
+      marginTop: 300
+    },
+    [theme.breakpoints.down('sm')]: {
+      marginTop: 350
+    }
+  },
+  content: {
+    height: 500,
+    width: 400,
+    [theme.breakpoints.down('sm')]: {
+      width: 400
+    }
+  },
+  cover: {
+    width: 300,
+    [theme.breakpoints.down('sm')]: {}
+  },
+  title: {
+    backgroundColor: 'black',
+    color: 'white',
+    paddingLeft: 5
+  },
+  contentBody: {
+    marginTop: 30
+  }
+}));
+
+function AgainstNazisInfoPage(props) {
+  const classes = useStyles();
+
+  const displayImage = () => {
+    if (isWidthUp('sm', props.width)) {
+      return (
+        <CardMedia
+          className={classes.cover}
+          image={imageInfoPage}
+          title="Live from space album cover"
+        />
+      );
+    }
+  };
+
+  return (
+    <div className={classes.root}>
+      <Card className={classes.card}>
+        {displayImage()}
+
+        <div>
+          <CardContent className={classes.content}>
+            <Typography component="h5" variant="h5" className={classes.title}>
+              Schaut nicht weg!
+            </Typography>
+            <Typography
+              variant="body1"
+              color="textSecondary"
+              className={classes.contentBody}
+            >
+              Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+              nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+              erat, sed diam voluptua. At vero eos et accusam et justo duo
+              dolores et ea rebum. Stet clita kasd gubergren, no sea takimata
+              sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit
+              amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
+              invidunt ut labore et dolore magna aliquyam erat, sed diam
+              voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
+              Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum
+              dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing
+              elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore
+              magna aliquyam erat, sed diam voluptua. At vero eos et accusam et
+              Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+              nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+              erat, sed diam voluptua. At vero eos et accusam et
+            </Typography>
+          </CardContent>
+        </div>
+      </Card>
+    </div>
+  );
 }
 
-export default AgainstNazisInfoPage;
+export default withWidth()(AgainstNazisInfoPage);
